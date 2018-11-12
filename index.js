2878e38c005a245893484e81f02f9b6f2a73fc88
Vasco Santos <vasco.santos@moxy.studio>
2018-11-12 14:41:23 +0000

    feat: add dht put and get

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const run = (async () => {
   console.info('node 0 repo properly configured')
 
   // Start node 0
-  await execa.shell('iptb start 0 --wait')
+  await execa.shell('iptb start 0 --wait -- --enable-dht-experiment')
 
   out = await execa.shell('iptb run 0 -- ipfs id --format="<addrs>"')
   const addr = out.stdout.split(/\r?\n/)[2]
@@ -28,14 +28,14 @@ const run = (async () => {
   for (let i = 1; i < n; i++) {
     await execa.shell(`iptb run ${i} -- ipfs config --json Bootstrap '["${addr}"]'`)
     // ipfs config --json Bootstrap '["/ip4/127.0.0.1/tcp/59163/ipfs/QmYEouqLdPepu5GiVL9LLxSByvophh8XaSq8wmrXWzupu6"]'
-    await execa.shell(`iptb start ${i} --wait`)
+    await execa.shell(`iptb start ${i} --wait -- --enable-dht-experiment`)
     console.info(`node ${i} repo properly configured and node started`)
   }
 
   // Put to the dht
-  out = await execa.shell('iptb run 1 -- ipfs dht put key value')
-  console.log('stdout: ', out.stdout)
+  await execa.shell('iptb run 1 -- ipfs dht put key value')
 
   // Get to the DHT
-
+  out = await execa.shell('iptb run 1 -- ipfs dht get key')
+  console.log('stdout dht get: ', out.stdout)
 })()
